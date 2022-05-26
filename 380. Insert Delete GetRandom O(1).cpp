5b81d8d711e42ecdff9e7b487fd5740c181f61ec
Vandana Chauhan <19bcs4532.cse@gmail.com>
2022-05-26 19:28:30 +0530

    Updated Array

--- 380. Insert Delete GetRandom O(1).cpp
+++ 380. Insert Delete GetRandom O(1).cpp
@@ -1,38 +1,40 @@
 class RandomizedSet {
-private:
-    vector<int> listArray; 
-    unordered_map<int,int> map;
+    vector<int> v;
+    unordered_map<int,int> mymap;
+    
 public:
-    RandomizedSet() {
-        
-    }
+    //initialize your data structure
+    RandomizedSet() { }
     
+    /*inserts a value to the set(array+map). return true if the set didn't already contain the specified element*/
     bool insert(int val) {
-       if(map.find(val) != map.end())
-       {
-           return false;
-       }
-       listArray.push_back(val);
-       map[val] = listArray.size()-1;
-       return true;
-    }
-    
-    bool remove(int val) {
-       if(map.find(val) == map.end())
-       {
-           return false;
-       }
-        int last_elem = listArray.back();
-        listArray[map[val]] = last_elem;
-        listArray.pop_back();
-        map[last_elem] = map[val];
-        map.erase(val);
-        return true;
+        if(mymap.find(val) != mymap.end())
+            return false;
         
+        v.pb(val); //insert the element at the end of the array
+        mymap[val] = v.size()-1;
+        return true;
     }
     
+    /*removes a value from the set(array +map). returns true if the set contained the specified element*/
+    bool remove(int val) {
+    
+        if(!mymap.count(val))
+            return false;
+    
+           mymap[v.back()]=mymap[val];//it will store ele as a key & index of ele as value of the key
+          swap(v.back(),v[mymap[val]]);//replaces the ele that v want to delete with the last ele of the array
+      
+      v.pop_back();//removing the last ele from the array
+      mymap.erase(val);//delete val from map also
+    
+  return true;
+}
+ 
+    /*get a random element from the set*/
     int getRandom() {
-        return listArray[rand()%listArray.size()];
+        return v[rand()%v.size()]; //rand() will return the index of random value generated & we're using % so that random value would be generated within the range of an array, i.e from 1 to size-1 
+        
     }
 };
 
